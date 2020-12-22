fbd44ecca7decc80f8ca7952e4993754bf346648
Pratik Jagade <pratik.21810767@viit.ac.in>
2020-12-22 15:43:48 +0530

    Table for cart added and some changes

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -9,14 +9,20 @@ import { FormsModule } from '@angular/forms';
 export class TestComponent implements OnInit {
 
   productName:string="";
-  products=["test","test"];
+  productPrice:number=0;
+  products=[
+    {name: "test" , price: 0},
+    {name: "test" , price: 0}
+  ];
 
   constructor() { }
 
   add():void
   {
-    this.products.push(this.productName);
+    //this.products.push(this.productName);
+    this.products.push({name:this.productName,price:this.productPrice});
     this.productName='';
+    this.productPrice=0;
   }
 
   ngOnInit(): void {
